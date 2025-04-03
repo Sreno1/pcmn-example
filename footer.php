7bfdc0a4d7d74c33ada343d06401fefe5614620f
stevenlykinsjr@gmail.com <stevenlykinsjr@gmail.com>
2025-04-03 17:15:40 -0400

    add bootstrap and local css and js directories

--- footer.php
+++ footer.php
@@ -2,11 +2,15 @@
 <?php get_sidebar(); ?>
 </div>
 <footer id="footer" role="contentinfo">
-<div id="copyright">
-&copy; <?php echo esc_html( date_i18n( __( 'Y', 'pcmnnurture' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
-</div>
+    <div id="copyright">
+        &copy; <?php echo esc_html( date_i18n( __( 'Y', 'pcmnnurture' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
+    </div>
 </footer>
 </div>
 <?php wp_footer(); ?>
+<!-- import Boostrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+        integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
+        crossorigin="anonymous"></script>
 </body>
 </html>
