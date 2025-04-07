af4e3a971129bb8678a2628161ffacfa48a2b8e2
stevenlykinsjr@gmail.com <stevenlykinsjr@gmail.com>
2025-04-06 23:40:17 -0400

    image-card block setup

---
+++ src/blocks/image-card/src/template.php
@@ -0,0 +1,12 @@
+<?php
+
+// Extract the 'title' attribute. Fallback to 'unknown'.
+$headingText = $attributes['headingText'];
+$contentText = $attributes['contentText'];
+$imageUrl    = $attributes['imageURL'];
+$imageAlt    = $attributes['imageAlt'];
+
+?>
+<div class="image-card">
+
+</div>
